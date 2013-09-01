badfbd7da33587674ebd6ff7cf935025c9a55fa9
Taiki ONO <taiks.4559@gmail.com>
2013-09-02 01:58:06 +0900

    Add Dual

--- monoid_n_foldable.hs
+++ monoid_n_foldable.hs
@@ -30,3 +30,21 @@ firstFoundRobot' = getFirst . fold $ fmap (First . (flip Map.lookup robots)) ["L
 
 -- use foldM
 firstFoundRobot'' = getFirst $ foldMap (First . (flip Map.lookup robots)) nameList
+
+
+--
+-- Dual
+dualOne = Dual $ Sum 3
+
+{-- With not commutative binary operation, the Dual idea will be clear --}
+
+listIsMonoid = [3, 4] `mappend` [5, 6]
+-- [3,4,5,6]
+dualMonoidList = (Dual $ [3, 4]) `mappend` (Dual $ [5, 6])
+-- Dual {getDual = [5,6,3,4]}
+
+{-- Dual commutates the binary operation in the Monoid --}
+
+-- Last is the Dual of First
+sameAsFirst = (Dual $ Last $ Just 4) `mappend` (Dual $ Last $ Just 5) `mappend` (Dual $ Last $ Just 6)
+-- Dual {getDual = Last {getLast = Just 4}}
