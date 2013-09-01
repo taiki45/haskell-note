3f16fd7362ce0f817828ed90c7190f5ef122a212
Taiki ONO <taiks.4559@gmail.com>
2013-09-02 01:18:05 +0900

    init

---
+++ monoid_n_foldable.hs
@@ -0,0 +1,25 @@
+import Data.Monoid
+import Data.Foldable
+import qualified Data.Map as Map
+
+lastOne = (Last $ Just 4) `mappend` (Last $ Just 3)
+-- Last {getLast = Just 3}
+
+withIdElem = (Last $ Just 4) `mappend` (Last $ Just 3) `mappend` (mempty $ Just 5)
+-- Last {getLast = Just 3}
+
+withFoldable = fold [Last $ Just 3, Last $ Just 4, Last Nothing]
+-- Just 4
+
+robots = Map.fromList [("Lisa", "345")
+                      ,("Bob", "234")]
+
+nameList = ["John", "Bob", "Lisa"]
+
+firstFoundRobot = getFirst . fold $ fmap (First . (flip Map.lookup robots)) nameList
+-- Just "234"
+firstFoundRobot' = getFirst . fold $ fmap (First . (flip Map.lookup robots)) ["L", "F"]
+-- Nothing
+
+-- use foldM
+firstFoundRobot'' = getFirst $ foldMap (First . (flip Map.lookup robots)) nameList
